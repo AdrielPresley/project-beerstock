7ee48970712477499831251af84d94a90c86af96
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-27 10:58:54 -0300

    Create BeerMapper.java

---
+++ src/main/java/one/dio/beerstock/mapper/BeerMapper.java
@@ -0,0 +1,17 @@
+package one.dio.beerstock.mapper;
+
+import one.dio.beerstock.dto.BeerDTO;
+import one.dio.beerstock.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+
+}
